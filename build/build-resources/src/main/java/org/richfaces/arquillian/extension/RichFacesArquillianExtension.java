a318041b1294261ddef15c011a0c8d09e4086bc4
Pavol Pitonak <ppitonak@redhat.com>
2015-02-13 16:49:22 +0100

    WindowResizer reintroduced

--- build/build-resources/src/main/java/org/richfaces/arquillian/extension/RichFacesArquillianExtension.java
+++ build/build-resources/src/main/java/org/richfaces/arquillian/extension/RichFacesArquillianExtension.java
@@ -2,6 +2,7 @@ package org.richfaces.arquillian.extension;
 
 import org.jboss.arquillian.core.spi.LoadableExtension;
 import org.jboss.arquillian.test.spi.enricher.resource.ResourceProvider;
+import org.richfaces.arquillian.browser.WindowResizer;
 import org.richfaces.arquillian.configuration.FundamentalTestConfiguratorObserver;
 import org.richfaces.arquillian.container.installation.ContainerInitializationObserver;
 import org.richfaces.arquillian.container.installation.ContainerInstaller;
@@ -20,5 +21,6 @@ public class RichFacesArquillianExtension implements LoadableExtension {
         builder.observer(ContainerInitializationObserver.class);
         builder.observer(ContainerInstaller.class);
         builder.observer(VerifyDeploymentTestability.class);
+        builder.observer(WindowResizer.class);
     }
 }
